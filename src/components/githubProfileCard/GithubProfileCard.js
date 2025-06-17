7fc1177e9c4002e9f47410c102e227ac746b280f
zaidi <aliakbarhadi2004@gmail.com>
2025-06-18 02:40:32 +0500

    removed redundant social media badges

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./GithubProfileCard.scss";
-import SocialMedia from "../../components/socialMedia/SocialMedia";
 import {isHireable} from "../../portfolio";
 import emoji from "react-easy-emoji";
 import {Fade} from "react-reveal";
@@ -46,7 +45,7 @@ export default function GithubProfileCard({prof}) {
                 Open for opportunities: {prof.hireable}
               </span>
             </div>
-            <SocialMedia />
+            
           </div>
           <div className="image-content-profile">
             <img
